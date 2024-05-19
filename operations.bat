f6dc6bdb1d726973c81fd73ccbeee6195759eb64
canoe <northboat@163.com>
2024-05-19 11:12:50 +0800

    update .bat

--- operations.bat
+++ operations.bat
@@ -4,12 +4,14 @@ echo [The Opeartions of This Dir]:
 echo	1.dev
 echo	2.build
 echo	3.push
+echo	4.pull
 
 set /p n=enter the operation number: 
 
 if "%n%"=="1"	npm run docs:dev
 if "%n%"=="2"	npm run docs:build
 if "%n%"=="3"	call :1
+if "%n%"=="4"	git pull
 
 pause
 goto :eof
